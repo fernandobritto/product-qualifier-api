3dbe1ec1a1db9ee71f097bcec7dab5b20ebd95cc
Fernando Britto <fernandobritto.web@gmail.com>
2018-12-31 12:06:42 -0200

    Initial commit

---
+++ head.php
@@ -0,0 +1,10 @@
+
+<meta charset="UTF-8">
+<meta name="keywords" content="HTML,CSS,JavaScript">
+<meta name="author" content="Fernando Britto">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title></title>
+        
+<link rel="stylesheet" href="css/style.css"/>
+
+
--- index.php
+++ index.php
@@ -2,13 +2,9 @@
 
 <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="keywords" content="HTML,CSS,JavaScript">
-        <meta name="author" content="Fernando Britto">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title></title>
-        
-        <link rel="stylesheet" href="css/style.css"/>
+        <?php 
+            include_once './head.php';
+        ?>
         
     </head>
     <body>
