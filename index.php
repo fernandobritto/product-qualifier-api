3ed0a0a618861ef21d72ba36a8ef1b4cbc19df78
Fernando Britto <fernandobritto.web@gmail.com>
2019-01-01 15:18:31 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <?php 
+            include_once './head.php';
+        ?>
+        
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
