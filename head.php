c34b402131a376a05af1ebc18b5a55c41dcd0e8e
Fernando Britto <fernandobritto.web@gmail.com>
2019-01-03 21:21:44 -0200

    Initial commit

--- head.php
+++ head.php
@@ -5,6 +5,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title></title>
         
-<link rel="stylesheet" href="css/style.css"/>
+<link rel="stylesheet" href="css/styles.css"/>
 
 
